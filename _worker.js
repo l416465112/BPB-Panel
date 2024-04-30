0570cbcfd8ea7811f4b314dfb253cba03d8d6b50
星空 <l416465112@users.noreply.github.com>
2024-04-30 22:38:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,11 +7,11 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'b27f8133-9d2b-49df-a249-3016561087ab';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
-const proxyIPs= ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
+const proxyIPs= [ 'workers.cloudflare.cyou','cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org',];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
