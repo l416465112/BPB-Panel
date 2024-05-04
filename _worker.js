95c4f9efdc3b6c7eace586ca60b106fcb9b4de8b
星空 <l416465112@users.noreply.github.com>
2024-05-05 02:13:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = 'f25b39cc-e275-4c99-a40a-2dac4f75f02e';
+let userID = '661a757c-86ea-4548-9d54-b5add882e810';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
